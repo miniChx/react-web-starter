a64d95b94f13e96548d79654783bc1cb65b68bc4
baoyinghai <yhbao@amarsoft.com>
2016-10-24 14:54:26 +0800

    Merge branch 'develop' of 192.168.64.208:maxwell/mx-web-boilerplate into develop
    # Conflicts:
    #	server/app.js
    #	server/package.json
    #	src/containers/index.js

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ var bodyParder = require('body-parser');
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-  //res.header('Access-Control-Allow-Origin', '*');
-  //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
   next();
 }
 
