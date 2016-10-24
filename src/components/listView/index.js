0d64333c685712feb4306b9a25611225d69ffa79
baoyinghai <yhbao@amarsoft.com>
2016-10-24 16:49:50 +0800

    feat: wraper demo

--- src/components/listView/index.js
+++ src/components/listView/index.js
@@ -6,9 +6,25 @@ import React from 'react';
 
 export default class ListView extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.initComponent = this.initComponent.bind(this);
+    this.state = {
+      data: '数据加载中'
+    }
+  }
+
+  initComponent(data) {
+    this.setState({
+      data: JSON.stringify(data, null, 4)
+    });
+  }
+
   render() {
     return (
-      <div>111111</div>
+      <div>
+        {this.state.data}
+      </div>
     );
   }
 };
