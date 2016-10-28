d0decc0b32f3136730e3414c28deed8487d712f9
yyge <yyge@amarsoft.com>
2016-10-28 15:41:13 +0800

    feat: add title style

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
 import React from 'react';
 import { connect } from 'react-redux';
 import { routerShape } from 'react-router';
+import Animate from 'rc-animate';
+import assign from 'object-assign';
 
 // import Header from './layout/Header';
 // import Footer from './layout/Footer';
@@ -22,6 +25,11 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.switchMenu = this.switchMenu.bind(this);
+    this.state = {
+      menuIsOpen: true,
+      enter: true
+    }
   }
 
   static contextTypes = {
@@ -49,13 +57,35 @@ class App extends React.Component {
     }
   }
 
+  switchMenu() {
+    this.setState({
+      menuIsOpen: !this.state.menuIsOpen,
+      enter: !this.state.enter
+    });
+  }
+
   render() {
+    const Div = (props) => {
+      const { style, show } = props;
+      const newStyle = assign({}, style, {
+        display: show ? '' : 'none',
+      });
+      return <Menu {...props} style={newStyle} />;
+    };
     if (this.props.session.token) {
       return (
         <div>
-          <Title />
-          <Row>
-            <Col span={4} ><Menu /></Col>
+          <Title switchMenu={this.switchMenu} />
+          <Row className={appStyle.appContent}>
+            <Col span={this.state.menuIsOpen ? 4 : 0} >
+              <Animate
+                component=""
+                showProp="show"
+                transitionName="move-left"
+              >
+                <Div show={this.state.enter}></Div>
+              </Animate>
+            </Col>
             <Col span={20} >{this.props.children}</Col>
           </Row>
 
