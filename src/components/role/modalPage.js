7c3cc6a9784feee62fd72c18c131a07e0129d196
xgwen <xgwen@amarsoft.com>
2016-11-10 18:12:26 +0800

    feat: create roledetail, addRole

--- src/components/role/modalPage.js
+++ src/components/role/modalPage.js
@@ -4,7 +4,7 @@
 /* eslint-disable */
 import React from 'react';
 import { autobind } from 'core-decorators';
-import { Modal, Button } from 'mxa';
+import { Modal, Button, Input, Row } from 'mxa';
 
 import styles from '../../styles/views/listview.less';
 
@@ -12,7 +12,8 @@ export default class ModalPage extends React.Component {
 
   static propTypes = {
     title: React.PropTypes.string,
-    record: React.PropTypes.object
+    mode: React.PropTypes.string,
+    record: React.PropTypes.object,
   };
 
   static defaultProps = {
@@ -49,17 +50,46 @@ export default class ModalPage extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Button className={styles.inlineButton} onClick={this.showModal}>{this.props.title}</Button>
-        <Modal title={this.props.title} visible={this.state.visible}
-               onOk={this.handleOk} onCancel={this.handleCancel}
-               okText="保存"
-        >
-          <p>some contents...</p>
-          <p>some contents...</p>
-        </Modal>
-      </div>
-    );
+    if (this.props.record) {
+      return (
+        <div>
+          <Button className={styles.inlineButton} onClick={this.showModal}>{this.props.title}</Button>
+          <Modal title={this.props.title} visible={this.state.visible}
+                 onOk={this.handleOk} onCancel={this.handleCancel}
+                 okText="保存"
+          >
+            <Row type="flex" justify="space-start">
+              <p>{'角色编号: '}</p>
+              <Input defaultValue={this.props.record.roleCode}/>
+            </Row>
+            <Row type="flex" justify="space-start">
+              <p>{'角色名称: '}</p>
+              <Input defaultValue={this.props.record.roleValue}/>
+            </Row>
+          </Modal>
+        </div>
+      );
+    } else if (this.props.mode === 'add'){
+      return (
+        <div>
+          <Button className={styles.inlineButton} onClick={this.showModal}>{this.props.title}</Button>
+          <Modal title={this.props.title} visible={this.state.visible}
+                 onOk={this.handleOk} onCancel={this.handleCancel}
+                 okText="增加"
+          >
+            <Row type="flex" justify="space-start">
+              <p>{'角色编号: '}</p>
+              <Input />
+            </Row>
+            <Row type="flex" justify="space-start">
+              <p>{'角色名称: '}</p>
+              <Input />
+            </Row>
+          </Modal>
+        </div>
+      );
+    }
+
+    return null;
   }
 }
