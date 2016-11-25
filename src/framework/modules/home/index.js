d19e9f235d4cf5e496d12cc39849c33bb408f7e8
alcat2008 <ge_yangyang@126.com>
2016-11-25 09:18:24 +0800

    feat: add district component

--- src/framework/modules/home/index.js
+++ src/framework/modules/home/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import { autobind } from 'core-decorators';
+import District from '../../../components/district';
 
-import homeStyle from '../../styles/views/home.less';
+import styles from '../../styles/views/home.less';
 
 export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    // initial state
+    this.state = {
+      province: '',
+      municipality: '',
+      district: '',
+    };
+  }
+
+  @autobind
+  _onChange(province, municipality, district) {
+    this.setState({ province, municipality, district });
+  }
+
   render() {
     return (
       <div className="home">
-        <div className={homeStyle.jump}>
+        <div className={styles.jump}>
           <a onClick={() => this.props.dispatch(push('/page_container/AccountList/render'))}>跳转到用户管理</a>
+        </div>
+        <div className={styles.district}>
+          <District onChange={this._onChange} />
+          <div>{'省： ' + this.state.province}</div>
+          <div>{'市： ' + this.state.municipality}</div>
+          <div>{'区： ' + this.state.district}</div>
         </div>
       </div>
     );
