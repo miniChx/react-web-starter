350582d45d659ad7ca7f87f4caf0924e5ea428df
alcat2008 <ge_yangyang@126.com>
2016-11-28 14:02:35 +0800

    chore: webpack-bundle-analyzer add but not work all the time

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,6 +12,8 @@ var paths = require('./paths');
 var getClientEnvironment = require('./env').getClientEnvironment;
 var appConfig = require('./env').getAppConfig();
 
+// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 function ensureSlash(path, needsSlash) {
   var hasSlash = path.endsWith('/');
   if (hasSlash && !needsSlash) {
@@ -191,6 +193,31 @@ module.exports = {
     ];
   },
   plugins: [
+    // new BundleAnalyzerPlugin({
+    //   // Can be `server`, `static` or `disabled`.
+    //   // In `server` mode analyzer will start HTTP server to show bundle report.
+    //   // In `static` mode single HTML file with bundle report will be generated.
+    //   // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+    //   analyzerMode: 'server',
+    //   // Port that will be used in `server` mode to start HTTP server.
+    //   analyzerPort: 8888,
+    //   // Path to bundle report file that will be generated in `static` mode.
+    //   // Relative to bundles output directory.
+    //   reportFilename: 'report.html',
+    //   // Automatically open report in default browser
+    //   openAnalyzer: true,
+    //   // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+    //   generateStatsFile: false,
+    //   // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+    //   // Relative to bundles output directory.
+    //   statsFilename: 'stats.json',
+    //   // Options for `stats.toJson()` method.
+    //   // For example you can exclude sources of your modules from stats file with `source: false` option.
+    //   // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+    //   statsOptions: null,
+    //   // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+    //   logLevel: 'info'
+    // }),
     // Makes the public URL available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
