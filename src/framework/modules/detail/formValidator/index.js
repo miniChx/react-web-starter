710765b40dbc870477f1e5531652f16ebe7e7606
baoyinghai <yhbao@amarsoft.com>
2016-12-19 17:16:08 +0800

    refactor: add type for validate

--- src/framework/modules/detail/formValidator/index.js
+++ src/framework/modules/detail/formValidator/index.js
@@ -1,15 +1,11 @@
 import checkFunc from './checkfunc';
+import TypeDic from './typeDic';
 
 // 表单校验
 const createRules = (record, form) => {
   const ret = [];
-
   const isRequired = { required: record.isRequired, message: '请输入' + record.description };
-  if (record.type === 'Date') {
-    isRequired.type = 'object';
-  } else if (record.type === 'Double' || record.type === 'Long' || record.type === 'Integer' || record.type === 'Float') {
-    isRequired.type = 'number';
-  }
+  isRequired.type = TypeDic[record.type];
   ret.push(isRequired);
   ret.push({ validator(rule, value, callback, source, options) {
     const validateType = record && record.formValidate && record.formValidate.validateType;
